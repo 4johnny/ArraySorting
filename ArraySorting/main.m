fdf896e318bd5ab5b17eef6b0e903b60ab8ee4a2
4johnny <4johnny@users.noreply.github.com>
2015-01-21 19:57:44 -0800

    Sort by Occurrences of a particular char.

--- ArraySorting/main.m
+++ ArraySorting/main.m
@@ -35,24 +35,49 @@ int main(int argc, const char * argv[]) {
 							 ];
 		MDLog(@"Strings: %@", strings);
 		
+		
 		// Sorted Alphabetically
 		NSArray* stringsByAlphabet = [strings sortedArrayUsingComparator:^(NSString* str1, NSString* str2) {
+			
 			return [str1 caseInsensitiveCompare:str2];
 		}];
 		MDLog(@"Sorted Alphabetically: %@", stringsByAlphabet);
 		
+		
 		// Sorted by Length
 		NSArray* stringsByLength = [strings sortedArrayUsingComparator: ^(NSString* str1, NSString* str2) {
+			
 			return (NSComparisonResult)(str2.length - str1.length);
 		}];
 		MDLog(@"Sorted Lengthwise: %@", stringsByLength);
 		
+		
 		// Sorted by Last Char
 		NSArray* stringsByLastChar = [strings sortedArrayUsingComparator: ^(NSString* str1, NSString* str2) {
-			return [[str1 substringFromIndex:str1.length - 1] caseInsensitiveCompare:[str2 substringFromIndex:str2.length - 1]];
+			
+			return [[str1 substringFromIndex:str1.length]
+					caseInsensitiveCompare:[str2 substringFromIndex:str2.length - 1]];
 		}];
 		MDLog(@"Sorted by Last Char: %@", stringsByLastChar);
-	
+		
+		// Sorted by Occurrences of 'e'
+		NSString* charStr = @"e";
+		NSArray* stringsByCharOccurrences = [strings sortedArrayUsingComparator: ^(NSString* str1, NSString* str2) {
+			
+			NSUInteger str1CountChar =
+			[str1.mutableCopy
+			 replaceOccurrencesOfString:charStr withString:charStr
+			 options:(NSStringCompareOptions)0 range:NSMakeRange(0, str1.length)];
+			
+			NSUInteger str2CountChar =
+			[str2.mutableCopy
+			 replaceOccurrencesOfString:charStr withString:charStr
+			 options:(NSStringCompareOptions)0 range:NSMakeRange(0, str2.length)];
+			
+			return (NSComparisonResult)(str2CountChar - str1CountChar);
+		}];
+		MDLog(@"Sorted by Occurrences of '%@': %@", charStr, stringsByCharOccurrences);
+		
 		
 	} // @autoreleasepool
 	
