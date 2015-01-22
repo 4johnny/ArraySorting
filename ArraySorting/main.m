caec88175a5091e9ad5853fe95864287ce239f3f
4johnny <4johnny@users.noreply.github.com>
2015-01-21 17:43:30 -0800

    Initial Commit

---
+++ ArraySorting/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ArraySorting
+//
+//  Created by Johnny on 2015-01-21.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
